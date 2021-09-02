c65fce793c63dfc4a5cae7069899604891d43c7c
Ruben Dugarte <eitocontrerasdu@gmail.com>
2021-09-02 18:24:41 -0300

    exercicio_3

---
+++ Aula 4/index.js
@@ -0,0 +1,7 @@
+console.log("Começando a contagem");
+
+for (let count = 1; count < 11; count++ ){
+    console.log(count);
+}
+
+console.log("FIM!");
---
+++ arquivo.js
@@ -0,0 +1,13 @@
+let nomesito = prompt("Escreva seu nome querido Dev:)");
+
+let sobre =  prompt("Penultimo_nome :D");
+
+let ida= prompt("Idade por favor :')");
+
+//console.log("Ola", nomesito, sobre, "Você tem", Number(ida), "anos" )
+
+console.table({
+    ola: nomesito,
+    sobrenome: sobre, 
+    você_tem: Number(ida)})
+
